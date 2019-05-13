6ebe1be07bf57f72d1c9fe6da5a0785dad8021c2
LDMGN <lgijsen@sphereon.com>
2019-05-13 16:47:40 +0200

    CUST-43 Add blockchain specific code

--- projects/alfresco-blockchain/src/public_api.ts
+++ projects/alfresco-blockchain/src/public_api.ts
@@ -4,3 +4,5 @@
 
 export * from './lib/alfresco-blockchain.service';
 export * from './lib/alfresco-blockchain.module';
+// export * from './lib/blockchain.actions';
+// export * from './lib/blockchain.effects';
