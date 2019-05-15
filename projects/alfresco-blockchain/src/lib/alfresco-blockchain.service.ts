35fd0f3e83787dd9ed0b85e40c9127a70406015b
LDMGN <lgijsen@sphereon.com>
2019-05-15 10:47:36 +0200

    CUST-43 Autoformat blockchain services

--- projects/alfresco-blockchain/src/lib/alfresco-blockchain.service.ts
+++ projects/alfresco-blockchain/src/lib/alfresco-blockchain.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlfrescoBlockchainService {
-  public foo() {
-    return 'bar';
+  public registerSelection(arg: any): Subject<string> {
+    const subj = new Subject<string>();
+    setTimeout(() => subj.next('Register'), 1000);
+    setTimeout(() => subj.next('Foo'), 1250);
+    setTimeout(() => subj.complete(), 1500);
+    return subj;
+  }
+
+  public verifySelection(arg: any): Subject<string> {
+    const subj = new Subject<string>();
+    subj.next('Verify');
+    subj.next('Bar');
+    return subj;
   }
 }
--- projects/alfresco-blockchain/src/lib/blockchain-proof/api/api/blockchain.service.ts
+++ projects/alfresco-blockchain/src/lib/blockchain-proof/api/api/blockchain.service.ts
@@ -1,6 +1,6 @@
 /**
  * Triall Agent for Alfresco API's
- * This is an API containing functions for blockchain integration with Alfresco.  
+ * This is an API containing functions for blockchain integration with Alfresco.
  *
  * OpenAPI spec version: 0.1
  * Contact: dev@sphereon.com
@@ -11,21 +11,16 @@
  */
 /* tslint:disable:no-unused-variable member-ordering */
 
-import { Inject, Injectable, Optional }                      from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams,
-         HttpResponse, HttpEvent }                           from '@angular/common/http';
-import { CustomHttpUrlEncodingCodec }                        from '../encoder';
-
-import { Observable }                                        from 'rxjs/Observable';
-
-import { ErrorResponse } from '../model/errorResponse';
+import { HttpClient, HttpEvent, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Inject, Injectable, Optional } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Configuration } from '../configuration';
 import { SendNodesToOffblocksRequest } from '../model/sendNodesToOffblocksRequest';
 import { SendNodesToOffblocksResponse } from '../model/sendNodesToOffblocksResponse';
 import { VerifyNodesRequest } from '../model/verifyNodesRequest';
 import { VerifyNodesResponse } from '../model/verifyNodesResponse';
 
-import { BASE_PATH, COLLECTION_FORMATS }                     from '../variables';
-import { Configuration }                                     from '../configuration';
+import { BASE_PATH } from '../variables';
 
 
 @Injectable()
@@ -35,7 +30,7 @@ export class BlockchainService {
     public defaultHeaders = new HttpHeaders();
     public configuration = new Configuration();
 
-    constructor(protected httpClient: HttpClient, @Optional()@Inject(BASE_PATH) basePath: string, @Optional() configuration: Configuration) {
+    constructor(protected httpClient: HttpClient, @Optional() @Inject(BASE_PATH) basePath: string, @Optional() configuration: Configuration) {
         if (basePath) {
             this.basePath = basePath;
         }
@@ -70,7 +65,7 @@ export class BlockchainService {
     public sendEntriesToOffblocks(nodeIds: SendNodesToOffblocksRequest, observe?: 'body', reportProgress?: boolean): Observable<SendNodesToOffblocksResponse>;
     public sendEntriesToOffblocks(nodeIds: SendNodesToOffblocksRequest, observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<SendNodesToOffblocksResponse>>;
     public sendEntriesToOffblocks(nodeIds: SendNodesToOffblocksRequest, observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<SendNodesToOffblocksResponse>>;
-    public sendEntriesToOffblocks(nodeIds: SendNodesToOffblocksRequest, observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+    public sendEntriesToOffblocks(nodeIds: SendNodesToOffblocksRequest, observe: any = 'body', reportProgress: boolean = false): Observable<any> {
         if (nodeIds === null || nodeIds === undefined) {
             throw new Error('Required parameter nodeIds was null or undefined when calling sendEntriesToOffblocks.');
         }
@@ -83,16 +78,16 @@ export class BlockchainService {
         ];
         let httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
         if (httpHeaderAcceptSelected != undefined) {
-            headers = headers.set("Accept", httpHeaderAcceptSelected);
+            headers = headers.set('Accept', httpHeaderAcceptSelected);
         }
 
         // to determine the Content-Type header
         let consumes: string[] = [
             'application/json;charset=UTF-8'
         ];
-        let httpContentTypeSelected:string | undefined = this.configuration.selectHeaderContentType(consumes);
+        let httpContentTypeSelected: string | undefined = this.configuration.selectHeaderContentType(consumes);
         if (httpContentTypeSelected != undefined) {
-            headers = headers.set("Content-Type", httpContentTypeSelected);
+            headers = headers.set('Content-Type', httpContentTypeSelected);
         }
 
         return this.httpClient.post<SendNodesToOffblocksResponse>(`${this.basePath}/alfresco-blockchain/sendToOffblocks/entries`,
@@ -116,7 +111,7 @@ export class BlockchainService {
     public verifyEntries(nodeIds: VerifyNodesRequest, observe?: 'body', reportProgress?: boolean): Observable<VerifyNodesResponse>;
     public verifyEntries(nodeIds: VerifyNodesRequest, observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<VerifyNodesResponse>>;
     public verifyEntries(nodeIds: VerifyNodesRequest, observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<VerifyNodesResponse>>;
-    public verifyEntries(nodeIds: VerifyNodesRequest, observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+    public verifyEntries(nodeIds: VerifyNodesRequest, observe: any = 'body', reportProgress: boolean = false): Observable<any> {
         if (nodeIds === null || nodeIds === undefined) {
             throw new Error('Required parameter nodeIds was null or undefined when calling verifyEntries.');
         }
@@ -129,16 +124,16 @@ export class BlockchainService {
         ];
         let httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
         if (httpHeaderAcceptSelected != undefined) {
-            headers = headers.set("Accept", httpHeaderAcceptSelected);
+            headers = headers.set('Accept', httpHeaderAcceptSelected);
         }
 
         // to determine the Content-Type header
         let consumes: string[] = [
             'application/json;charset=UTF-8'
         ];
-        let httpContentTypeSelected:string | undefined = this.configuration.selectHeaderContentType(consumes);
+        let httpContentTypeSelected: string | undefined = this.configuration.selectHeaderContentType(consumes);
         if (httpContentTypeSelected != undefined) {
-            headers = headers.set("Content-Type", httpContentTypeSelected);
+            headers = headers.set('Content-Type', httpContentTypeSelected);
         }
 
         return this.httpClient.post<VerifyNodesResponse>(`${this.basePath}/alfresco-blockchain/verify/entries`,
